d40ec7ba3b4dad29c38bfbc5f94f6326c7537d8d
beilmouz <nutbill30@gmail>
2024-12-03 10:37:35 +0700

    fix: port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import http from "http";
 import { env } from "process";
 import app from "./app.js";
 
-const port = env.PORT || 3036;
+const port = env.PORT;
 
 const server = http.createServer(app);
 // const server = https.createServer(app, options);
